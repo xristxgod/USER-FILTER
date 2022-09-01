fd593f88995ce72940887ded732eac5687887fa6
Mamedov Murad <mamedov_99b@mail.ru>
2022-09-01 09:32:38 +0300

    [ADD] new query schema

--- src/schemas.py
+++ src/schemas.py
@@ -2,7 +2,20 @@ import decimal
 from typing import Optional, Dict
 from datetime import datetime
 
-from pydantic import BaseModel, Field, EmailStr, ValidationError, validator
+from fastapi import HTTPException, status
+from dateutil import parser
+
+from pydantic import BaseModel, Field, EmailStr, validator
+
+
+def is_iso_8601(date: str) -> datetime:
+    try:
+        return parser.parse(date)
+    except parser._parser.ParserError as error:
+        raise HTTPException(
+            detail="The date must be in the format: ISO 8601",
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY
+        )
 
 
 class QueryUser(BaseModel):
@@ -10,22 +23,66 @@ class QueryUser(BaseModel):
     company: Optional[str] = Field(description="Company", default=None)
     gender: Optional[str] = Field(description="User gender", default=None)
     jobTitle: Optional[str] = Field(description="Job title", default=None)
-    joinDateStart: Optional[datetime] = Field(default=None)
-    joinDateEnd: Optional[datetime] = Field(default=None)
+    joinDateStart: Optional[str] = Field(default=None)      # ISO 8601
+    joinDateEnd: Optional[str] = Field(default=None)        # ISO 8601
     salaryStart: Optional[decimal.Decimal] = Field(default=None)
     salaryEnd: Optional[decimal.Decimal] = Field(default=None)
 
     @validator("gender")
-    def valid_gender(self, gender: str):
+    def valid_gender(cls, gender: str):
+        if gender is None:
+            return None
         if gender not in ["female", "male", "other"]:
-            raise ValidationError("Gender can only be: female, male, other")
+            raise HTTPException(
+                detail="Gender can only be: female, male, other",
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY
+            )
         return gender
 
     @validator("joinDateStart")
-    def valid_join_date(self, join_date_start: datetime, values: Dict):
-        if join_date_start < values["joinDateEnd"]:
-            raise ValidationError()
-        return join_date_start
+    def valid_join_date_start(cls, join_date_start: Optional[str]):
+        if join_date_start is None:
+            return None
+        return is_iso_8601(join_date_start)
+
+    @validator("joinDateEnd")
+    def valid_join_date_end(cls, join_date_end: str, values: Dict):
+        if join_date_end is None and values.get("joinDateStart"):
+            return values["joinDateStart"]
+        elif join_date_end is None and not values.get("joinDateStart"):
+            return None
+        elif join_date_end and not values.get("joinDateStart"):
+            return is_iso_8601(join_date_end)
+        else:
+            end = is_iso_8601(join_date_end)
+            if end < values["joinDateStart"]:
+                raise HTTPException(
+                    detail="The start date must be less than the end date!",
+                    status_code=status.HTTP_422_UNPROCESSABLE_ENTITY
+                )
+            return is_iso_8601(join_date_end)
+
+    @validator("salaryStart")
+    def valid_salary_start(cls, salary_start: decimal.Decimal):
+        if salary_start is None:
+            return None
+        return salary_start
+
+    @validator("salaryEnd")
+    def valid_salary_end(cls, salary_end: decimal.Decimal, values: Dict):
+        if salary_end is None and values.get("salaryStart"):
+            return values.get("salaryStart")
+        elif salary_end is None and not values.get("salaryStart"):
+            return None
+        elif salary_end and not values.get("salaryStart"):
+            return salary_end
+        else:
+            if salary_end < values.get("salaryStart"):
+                raise HTTPException(
+                    detail="The starting salary must be less than the final salary!",
+                    status_code=status.HTTP_422_UNPROCESSABLE_ENTITY
+                )
+            return salary_end
 
 
 class ResponseUser(BaseModel):
