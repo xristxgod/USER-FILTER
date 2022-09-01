5a6eb9fce62f23e01600d6886c64afbf5784a192
Mamedov Murad <mamedov_99b@mail.ru>
2022-09-01 10:55:52 +0300

    [FIX]

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ FILE = os.path.join(ROOT_DIR, "employees.json")
 
 
 class Config:
-    MONGODB_NAME = os.getenv("MONGODB_NAME", "test")
-    MONGODB_COLLECTION = os.getenv("MONGODB_COLLECTION", "users")
+    MONGODB_NAME = os.getenv("MONGODB_NAME", "dev_user_db")
+    MONGODB_COLLECTION = os.getenv("MONGODB_COLLECTION", "dev_users")
     MONGODB_URL = os.getenv("MONGODB_URL", "mongodb://docker:mongopw@localhost:49154")
