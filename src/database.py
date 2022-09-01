244dbd99de37a4ea54ef0cc4d4b1f44a4cb002d5
Mamedov Murad <mamedov_99b@mail.ru>
2022-09-01 10:25:12 +0300

    [ADD] new class Filter and two methods

--- src/database.py
+++ src/database.py
@@ -1,9 +1,31 @@
-from typing import List, Dict
+import decimal
+from datetime import datetime
+from typing import Optional, List, Dict
 
 import pymongo
 
-from src.schemas import ResponseUser
+from src.schemas import ResponseUser, QueryUser
 from config import Config, FILE
+
+
+class Filter:
+    @staticmethod
+    def filter_integer(start: Optional[int] = None, end: Optional[int] = None) -> Dict:
+        if start is None and end is None:
+            return {}
+        elif start and end is None:
+            return {"age": {"$gt": start - 1}}
+        elif start is None and end:
+            return {"age": {"$lt": end + 1}}
+        elif start == end:
+            return {"age": start}
+        else:
+            return {"age": {"$gt": start, "$lt": end}}
+
+    @staticmethod
+    def filter_datetime(start: Optional[datetime] = None, end: Optional[datetime] = None) -> Dict:
+        if start is None and end is None:
+            return {}
 
 
 class Database:
@@ -18,14 +40,26 @@ class Database:
         self.collection = self.db.get_collection(Config.MONGODB_COLLECTION)
 
     def get_all_data(self) -> List[ResponseUser]:
-        users = self.collection.find({})
-        data = []
-        for user in users:
-            data.append(ResponseUser(**user))
-        return data
+        return [ResponseUser(**user) for user in self.collection.find({})]
 
-    def get_filter(self):
-        pass
+    def get_filter(self, query: QueryUser) -> List[ResponseUser]:
+        """Search users by filters"""
+        _filters = {}
+        if query.company is not None:
+            _filters.update({"company": query.company})
+        if query.gender is not None:
+            _filters.update({"gender": query.gender})
+        if query.jobTitle is not None:
+            _filters.update({"job_title": query.jobTitle})
+        if query.ageStart or query.ageEnd:
+            _filters.update(Filter.filter_integer(start=query.ageStart, end=query.ageEnd))
+        if query.joinDateStart or query.joinDateEnd:
+            _filters.update(Filter.filter_datetime(start=query.joinDateStart, end=query.joinDateEnd))
+        if query.salaryStart or query.salaryEnd:
+            _filters.update(Filter.filter_integer(start=query.salaryStart, end=query.salaryEnd))
+
+        users = [ResponseUser(**user) for user in self.collection.find(_filters)]
+        return users
 
     def insert_data(self, data: List[Dict]) -> bool:
         self.collection.insert_many(data)
