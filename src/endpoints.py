d04ee7e1b43eabbefb3fad047e49abb51785443e
Mamedov Murad <mamedov_99b@mail.ru>
2022-09-01 10:53:23 +0300

    [FIX] endpoints

--- src/endpoints.py
+++ src/endpoints.py
@@ -6,15 +6,14 @@ from .schemas import ResponseUser, QueryUser
 from .database import db
 
 
-router = APIRouter(
-    prefix="/user",
-    tags=["USER"]
-)
+router = APIRouter(prefix="/user", tags=["USER"])
 
 
-@router.get(
-    "/filter",
-    # response_model=List[ResponseUser]
-)
+@router.get("/filter", response_model=List[ResponseUser], description="Get filtered and sorted data data!")
 async def get_users(query: QueryUser = Depends()):
-    print(query)
+    return db.get_filter(query=query)
+
+
+__all__ = [
+    "router"
+]
