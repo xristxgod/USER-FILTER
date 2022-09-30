a24aa8fa2f26ce91fb55e31ff30d6caa0098769e
Mamedov Murad <mamedov_99b@mail.ru>
2022-09-30 13:06:29 +0300

    [ADD] filter schema

--- backend/src/rest/schemas.py
+++ backend/src/rest/schemas.py
@@ -1,37 +1,58 @@
+import decimal
 from enum import Enum
-from typing import Optional, Dict
+from datetime import datetime
+from typing import Union, Optional, Dict
 
 from fastapi import HTTPException, status
 from pydantic import BaseModel, Field, validator
 
 
+NUMERIC = Union[decimal.Decimal, float, int]
+
+
 class GenderEnum(Enum):
-    pass
+    female = "female"
+    male = "male"
+    other = "other"
 
 
 class QueryUserFilter(BaseModel):
     ageStart: Optional[int] = Field(description="Start with", default=None)
     ageEnd: Optional[int] = Field(description="Finish on", default=None)
     company: Optional[str] = Field(description="Company", default=None)
-    gender: Optional[str] = Field(description="User gender", default=None)
+    gender: Optional[GenderEnum] = Field(description="User gender", default=None)
     jobTitle: Optional[str] = Field(description="Job title", default=None)
-    joinDateStart: Optional[str] = Field(description="Start with", default=None)
-    joinDateEnd: Optional[str] = Field(description="Finish on", default=None)
-    salaryStart: Optional[int] = Field(description="Start with", default=None)
-    salaryEnd: Optional[int] = Field(description="Finish on", default=None)
+    joinDateStart: Optional[datetime] = Field(description="Start with", default=None)
+    joinDateEnd: Optional[datetime] = Field(description="Finish on", default=None)
+    salaryStart: Optional[NUMERIC] = Field(description="Start with", default=None)
+    salaryEnd: Optional[NUMERIC] = Field(description="Finish on", default=None)
 
     @validator("ageEnd")
-    def valid_age_end(cls, age: int, values: Dict):
-        if age is None and not values.get("ageStart"):
-            return None
-        elif age and not values.get("ageStart"):
-            return age
-        elif age is not None and values.get("ageStart") is not None and age < values.get("ageStart"):
+    def valid_age(cls, age: int, values: Dict):
+        if (age and values["ageStart"]) and values["ageStart"] > age:
             raise HTTPException(
                 detail="The starting age must be less than the end!",
                 status_code=status.HTTP_422_UNPROCESSABLE_ENTITY
             )
         return age
+
+    @validator("joinDateEnd")
+    def valid_join_data(cls, join_data: datetime, values: Dict):
+        if (join_data and values["joinDateStart"]) and values["joinDateStart"] > join_data:
+            raise HTTPException(
+                detail="The start date must be less than the end date!",
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY
+            )
+        return join_data
+
+    @validator("salaryEnd")
+    def valid_salary(cls, salary: NUMERIC, values: Dict):
+        if (salary and values["salaryStart"]) and values["salaryStart"] > salary:
+            raise HTTPException(
+                detail="The starting salary must be less than the final salary!",
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY
+            )
+        return salary
 
 
 class ResponseSuccessfully(BaseModel):
@@ -46,5 +67,6 @@ class ResponseSuccessfully(BaseModel):
 
 
 __all__ = [
+    "QueryUserFilter",
     "ResponseSuccessfully"
 ]
