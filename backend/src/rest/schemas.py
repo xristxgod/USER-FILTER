e46a68555bd443651439c5f5715d919a136ef458
Mamedov Murad <mamedov_99b@mail.ru>
2022-09-30 10:14:44 +0300

    [ADD] new schemas

---
+++ backend/src/rest/schemas.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel, Field
+
+
+class ResponseSuccessfully(BaseModel):
+    successfully: bool = Field(default=True)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "successfully": True
+            }
+        }
